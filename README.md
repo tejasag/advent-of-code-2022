63fa248d60655ea79a2929f52012ab0800a381d2
Tejas Agarwal <tejasagarwalbly@gmail.com>
2022-12-06 11:02:03 +0530

    utils

---
+++ README.md
@@ -0,0 +1,7 @@
+# `advent-of-code-2022`
+
+My solutions for [Advent of code 2022](https://adventofcode.com)!
+
+### LICENSE
+
+[MIT](LICENSE.md)
--- day1/sol.py
+++
@@ -1,6 +0,0 @@
-with open("input.txt", "r") as f:
-  i = f.read() 
-  sol = sorted(map(lambda x: sum(map(int, x.split("\n"))), i.split("\n\n")[:-1]), reverse=True)
-  print(sol[0])
-  print(sol[0] + sol[1] + sol[2])
-  
